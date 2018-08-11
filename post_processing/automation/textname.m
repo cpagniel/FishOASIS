81831666f749f43e19b50aa883459e7a16d4daf6
caihelen <helen.cai@yale.edu>
2018-08-11 09:13:35 -0700

    Windows is special

--- post_processing/automation/textname.m
+++ post_processing/automation/textname.m
@@ -12,7 +12,7 @@ function name = textname(files, current)
 
 
 temp = erase(string(files(current).name), '.jpg');
-name = strcat(files(current).folder, '/', temp, '.txt');
+name = strcat(files(current).folder, '\', temp, '.txt');
 
 
 end
