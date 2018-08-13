9f9df96dd5662511ac7a0f4692229a43b2f5a060
caihelen <helen.cai@yale.edu>
2018-08-12 23:50:04 -0700

    add boolean flags

--- post_processing/automation/run_detections.m
+++ post_processing/automation/run_detections.m
@@ -3,6 +3,11 @@
 % classification (using fishnet). 
 % Generates one text file for each raw image. 
 
+% Change these boolean flags as needed
+TRAIN = 1;
+DETECT = 1;
+CLASSIFY = 1;
+
 % Classes of detection that fishnet has been trained on. Should correspond
 % with those listed in classes.txt.
 classes = categorical({'blacksmith' 'flora' 'garibaldi' 'halfmoon' 'kelp' ...
@@ -14,6 +19,13 @@ data_dir = uigetdir('', 'Select folder containing raw images.');
 fprintf('Loading images from ');
 fprintf('%s', data_dir);
 
+% Train network if necessary
+if TRAIN
+    fprintf('\nTraining new classification network.\n');
+    addpath('./fishnet');
+    train_classifier
+end
+
 % Get classification network of choice
 fprintf('\nSelect classification network.\n');
 [network, pathname] = uigetfile('*.mat', 'Select classification network.');
@@ -22,54 +34,58 @@ fprintf('Loaded detection network: ');
 fprintf(network);
 
 % Run detections on each image
-fprintf('\nRunning detection on images: \n');
-n_images = size(dir([fullfile(data_dir), '\*.jpg']));
-n_images = n_images(1);
-for i = 1:n_images
-    detect_fish(data_dir, i);
+if DETECT
+    fprintf('\nRunning detection on images: \n');
+    n_images = size(dir([fullfile(data_dir), '\*.jpg']));
+    n_images = n_images(1);
+    for i = 1:n_images
+        detect_fish(data_dir, i);
+    end
 end
 
 % Run classification on each image
-fprintf('Classifying detections.\n');
-files = dir([fullfile(data_dir), '\*.jpg']);
-for i = 1:n_images
-    
-    % Read image
-    I = imread(files(i).name);
-    
-    % Read contents of corresponding text file
-    fileID = fopen( textname(files,i), 'r+'); % check permissions here
-    dtns = fscanf(fileID, '%d', [4, inf])' ;
-    fclose(fileID);
-    
-    % Check if the file is empty
-    if isempty(dtns)
-        continue
-    else
-        % Iterate through each detection
-        s = size(dtns);
-        for j = 1:s(1)
-            % Crop and resize image for classification
-            rect = dtns(j,:);
-            rect(3) = max(rect(3), rect(4));
-            rect(4) = max(rect(3), rect(4));
-            J = imcrop(I, rect);
-            J = imresize(J, [227 227]);
-            pred = classify(fishnet, J);
-            fprintf(strcat(string(pred), '\n'));
-            
-            % Add species code
-            [C, index] = intersect(classes, pred);
-            dtns(j,5) = index;
+if CLASSIFY
+    fprintf('Classifying detections.\n');
+    files = dir([fullfile(data_dir), '\*.jpg']);
+    for i = 1:n_images
+
+        % Read image
+        I = imread(files(i).name);
+
+        % Read contents of corresponding text file
+        fileID = fopen( textname(files,i), 'r+'); % check permissions here
+        dtns = fscanf(fileID, '%d', [4, inf])' ;
+        fclose(fileID);
+
+        % Check if the file is empty
+        if isempty(dtns)
+            continue
+        else
+            % Iterate through each detection
+            s = size(dtns);
+            for j = 1:s(1)
+                % Crop and resize image for classification
+                rect = dtns(j,1:4);
+                rect(3) = max(rect(3), rect(4));
+                rect(4) = max(rect(3), rect(4));
+                J = imcrop(I, rect);
+                J = imresize(J, [227 227]);
+                pred = classify(fishnet, J);
+                fprintf(strcat(string(pred), '\n'));
+
+                % Add species code
+                [C, index] = intersect(classes, pred);
+                dtns(j,5) = index;
+            end
+
+            % Write contents of file back into text file
+            % Each row is formatted as:
+            %       [left coordinate] [top coordinate] [width] [height] [class code]
+            dlmwrite( textname(files,i), dtns, ' ');
+
         end
-        
-        % Write contents of file back into text file
-        % Each row is formatted as:
-        %       [left coordinate] [top coordinate] [width] [height] [class code]
-        dlmwrite( textname(files,i), dtns, ' ');
-        
+
     end
-    
 end
 
 fprintf('Done with detection & classification. \n');
